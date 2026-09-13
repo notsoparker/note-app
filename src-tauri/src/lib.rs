5a4b5b71e587d3e4b7d0e245da4a786d522241d0
parkerasnicar <parkerasnicar@gmail.com>
2026-09-13 20:53:40 +1000

    NA-001: Simple Text Field Creation
    + Text field created
    + Hotkey to open and close field
    + Small styles.css additions

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,73 @@
+use tauri::Manager;
+use tauri_plugin_global_shortcut::{GlobalShortcutExt, ShortcutState};
+
+#[cfg(target_os = "macos")]
+use cocoa::appkit::NSWindow;
+#[cfg(target_os = "macos")]
+use cocoa::base::id;
+#[cfg(target_os = "macos")]
+use objc::{msg_send, sel, sel_impl};
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+#[cfg(target_os = "macos")]
+fn round_window_corners(window: &tauri::WebviewWindow, radius: f64) {
+    if let Ok(ns_window) = window.ns_window() {
+        unsafe {
+            let ns_window = ns_window as id;
+            let content_view: id = ns_window.contentView();
+            let _: () = msg_send![content_view, setWantsLayer: true];
+            let layer: id = msg_send![content_view, layer];
+            let _: () = msg_send![layer, setCornerRadius: radius];
+            let _: () = msg_send![layer, setMasksToBounds: true];
+        }
+    }
+}
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+    tauri::Builder::default()
+        .plugin(tauri_plugin_opener::init())
+        .plugin(
+            tauri_plugin_global_shortcut::Builder::new()
+                .with_handler(|app, shortcut, event| {
+                    if event.state() == ShortcutState::Pressed {
+                        println!("Hotkey pressed: {:?}", shortcut);
+                        if let Some(window) = app.get_webview_window("main") {
+                            if window.is_visible().unwrap_or(false) {
+                                let _ = window.hide();
+                            } else {
+                                let _ = window.show();
+                                let _ = window.set_focus();
+                            }
+                        }
+                    }
+                })
+                .build(),
+        )
+        .setup(|app| {
+            let shortcut = "CmdOrCtrl+Shift+`";
+            app.global_shortcut().register(shortcut)?;
+
+            if let Some(window) = app.get_webview_window("main") {
+                #[cfg(target_os = "macos")]
+                round_window_corners(&window, 12.0);
+
+                let window_clone = window.clone();
+                window.on_window_event(move |event| {
+                    if let tauri::WindowEvent::CloseRequested { api, .. } = event {
+                        api.prevent_close();
+                        let _ = window_clone.hide();
+                    }
+                });
+            }
+
+            Ok(())
+        })
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
