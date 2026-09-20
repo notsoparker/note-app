ab5895e8e0807b00f400e2e7cb195b759884eed3
parkerasnicar <parkerasnicar@gmail.com>
2026-09-20 11:11:50 +1000

    NA-006: List/Save Visibility State Functionality

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,15 @@ const MAX_NOTE_LENGTH = 75;
 
 let noteEl: HTMLTextAreaElement | null;
 let charCountEl: HTMLElement | null;
+let saveBtnEl: HTMLButtonElement | null;
+let listBtnEl: HTMLButtonElement | null;
+
+function updateListSaveButtonState() {
+  if (!noteEl || !saveBtnEl || !listBtnEl) return;
+  const hasText = noteEl.value.trim().length > 0;
+  saveBtnEl.hidden = !hasText;
+  listBtnEl.hidden = hasText;
+}
 
 function updateCharCount() {
   if (!noteEl || !charCountEl) return;
@@ -13,6 +22,14 @@ function updateCharCount() {
 window.addEventListener("DOMContentLoaded", () => {
   noteEl = document.querySelector("#textarea");
   charCountEl = document.querySelector("#char-count");
-  noteEl?.addEventListener("input", updateCharCount);
+  saveBtnEl = document.querySelector("#save-btn");
+  listBtnEl = document.querySelector("#list-btn");
+
+  noteEl?.addEventListener("input", () => {
+    updateCharCount();
+    updateListSaveButtonState();
+  });
+
   updateCharCount();
+  updateListSaveButtonState();
 });
